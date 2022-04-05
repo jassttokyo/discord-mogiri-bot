049197f4c82e08e11e9177ecb06b0ce65f86f1df
ropQa <530lotus@gmail.com>
2022-04-05 21:53:10 +0900

    Implement mogiri()

--- commands/mogiri.js
+++ commands/mogiri.js
@@ -5,10 +5,37 @@ module.exports = {
 		.setName('mogiri')
 		.setDescription('mogiri command'),
 	async execute(interaction) {
-		mogiri();
+		mogiri(interaction.member);
+		interaction.reply('done.');
 	},
 };
 
-function mogiri() {
-	//Do mogiri
+/**
+ * ロールを付与する
+ * @param {GuildMember} member : 付与対象のメンバー
+ */
+function mogiri(member) {
+	// 既に付与済みであれば処理をスキップする
+	if (member.roles.cache.has(process.env.participantRoleId)) return;
+
+	if (checkParticipant(member.user.username)) {
+		member.roles.add(process.env.participantRoleId);
+		console.log("Grant role to " + member.user.username);
+	}
+	if (checkParticipant(member.nickname)) {
+		member.roles.add(process.env.participantRoleId);
+		console.log("Grant role to " + member.nickname);
+	}
 }
+
+/**
+ * 
+ * @param {String} name 
+ * @returns 付与対象であればtrue
+ */
+function checkParticipant(name) {
+	if (!name) return false;
+	if (name.match(/.*To2-\d{5}.*/)) return true;
+}
+
+module.exports = checkParticipant;
---
+++ tests/mogiri.test.js
@@ -0,0 +1,22 @@
+const checkParticipant = require('../commands/mogiri');
+
+test('valid member pass the check', () => {
+	expect(checkParticipant('To2-12345')).toBeTruthy();
+});
+
+test('valid member with emoji pass the check', () => {
+	expect(checkParticipant('🍣🍣To2-12345🍺🍺')).toBeTruthy();
+});
+
+test('valid member with empty pass the check', () => {
+	expect(checkParticipant('  To2-12345  ')).toBeTruthy();
+	expect(checkParticipant('　　To2-12345　　')).toBeTruthy();
+});
+
+test('invalid member do not pass the check', () => {
+	expect(checkParticipant('lorem ipsum')).toBeFalsy();
+});
+
+test('invalid member do not pass the check', () => {
+	expect(checkParticipant('To2 - 12345')).toBeFalsy();
+});
